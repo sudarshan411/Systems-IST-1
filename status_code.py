26fd3a988da79bbb5b0f860e1f2a51c54528aad0
DeltaCube23 <adithyarajesh01@gmail.com>
2021-05-27 15:48:28 +0400

    added files for maintaining list of websites and log of results

--- status_code.py
+++ status_code.py
@@ -3,23 +3,47 @@
 import requests
 import click
 import time
+from datetime import datetime
 
-# python3 status_code.py youtube.com 5
-# will fetch status code of website after every timer seconds
+# python3 status_code.py 5 3
+# will fetch status code of websites in the list 
+# after intervals of 5 seconds and this program
+# will run for 3 iterations
 
 @click.command()
-@click.argument('website')
-@click.argument('timer', default=60)
-def main(website, timer):
-    addr = "https://" + website
-    print(addr)
-    while True:
-        resp = requests.get(addr)
-        if resp.status_code == 200:
-            print("The website is working")
-        else:
-            print("Website has error : " + resp.status_code)
+@click.argument('timer', default=10)
+@click.argument('count', default=1)
+def main(timer, count):
+    # list.txt contains the list of websites to be tracked
+    file1 = open('list.txt', 'r')
+    lines = file1.readlines()
+    file1.close()
+
+    # to store the all the results
+    file2 = open('log.txt', 'w')
+
+    # keep running for count iterations
+    while count > 0:
+        for line in lines:
+            addr = "https://" + line.strip()
+            print(addr)
+
+            resp = requests.get(addr)
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
+            if resp.status_code == 200:
+                str = current_time + " : " + addr + " is working\n"
+                file2.write(str)
+            else:
+                str = current_time + " : " + addr + " has error : " + resp.status_code + "\n"
+                file2.write(str)
+
+        # repeat after timer seconds
         time.sleep(timer)
+        count -= 1
+    
+    file2.close()
 
 if __name__ == "__main__":
     main()
